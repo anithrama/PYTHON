8be2aa6bc8c52a666eae07409d2c530bbb3cf8cd
anithrama <136320706+anithrama@users.noreply.github.com>
2023-10-27 13:00:49 +0530

    add file

---
+++ number.py
@@ -0,0 +1,29 @@
+a=5
+
+for i in range(1,a+1):
+    for j in range(1,2*i,2):
+        print(j,end=" ")
+    print()
+
+
+num=1
+for i in range(5):
+    for j in range(i+1):
+        print(num,end=" ")
+        num+=2
+    print()
+
+k=3
+for i in range(1,k+1):
+    for j in range(i,0,-1):
+      print(j,end="")
+    print()
+
+
+n=5
+for i in range(1,n+1):
+   for j in range(1,i+1):
+    print(1 if j%2==1 else 2,end=" ")
+   print()
+
+
